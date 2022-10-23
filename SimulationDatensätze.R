718c3d639c504ce87dca73e8c5ce8f12df88e380
Sara Schiesberg <sara.schiesberg@planumnull.de>
2022-10-23 17:43:13 +0200

    Redaktionelles

--- SimulationDatensätze.R
+++ SimulationDatensätze.R
@@ -3,16 +3,15 @@
 # Ziel: Nimmt eine Excel-Liste mit den Namen der Prüflinge und eine txt mit den Namen der Orks. Gibt für jeden Prüfling eine csv mit Merkmalen zu den verschiedenen Orks. 
 
 # Pakete laden
-# library(readr)
+library(fGarch) # für schiefen Verteilungen
 library(readxl)
 library(tidyverse)
-library(fGarch) # für schiefen Verteilungen
 
 # Liste der Prüflinge einlesen
 prueflinge <- read_excel("Prüflinge.xlsx", range = "B1:C28")
 prueflinge$pruefling <- paste(prueflinge$Nachname, "_", prueflinge$Vorname, sep= "")
 
-# Diese Spalten sollen in jeder Ergebnistabelle dieselben Daten enthalten: 
+# Diese Spalten enthalten in jeder Ergebnistabelle dieselben Werte: 
 id <- seq(1:100)
 # Spalte Namen
 orknamen <- read_delim("Orknamen.txt", delim = "\t", escape_double = FALSE, col_names = FALSE,  trim_ws = TRUE)
@@ -20,13 +19,13 @@ orknamen <- rename(orknamen, name = X1)
 # Dataframe erzeugen
 orks <- as.data.frame(cbind(id, orknamen))
 
-# Jede Ergebnistabelle soll die folgenden Mermalsausprägungen beinhalten:
+# Jede Ergebnistabelle beinhaltet die folgenden Merkmalsausprägungen:
 kanni <- c("sehr oft", "oft", "selten", "noch nie")# für Kannibalismus
 krumm <- c("X-Beine", "O-Beine", "S-Beine")# für Krummbeinigkeit
 clans <- c("Goffs", "Snakebites", "Evil Sunz")# für Clans
 geschl <- c("männlich", "weiblich", "divers")
 
-# Für jeden Prüfling wird nun eine Tabelle erzeugt. Diese nun definierten Spalten haben in jeder Ergebnistabelle verschiedene Werte: 
+# Für jeden Prüfling wird nun eine Tabelle erzeugt. Die nun definierten Spalten enthalten individuelle Werte: 
 for (i in prueflinge$pruefling) {
   # Spalte Clan
   orks$clan <- sample(clans, 100, replace = TRUE) 
@@ -42,7 +41,7 @@ for (i in prueflinge$pruefling) {
                             kannibalismus == "oft" ~ round(rnorm(100, 50, 5),2),
                             kannibalismus == "selten" ~ round(rnorm(100, 40, 5),2),
                             kannibalismus == "noch nie" ~ round(rnorm(100, 30, 5),2)))
-    # Körperhöhe in Abhängigkeit von der Clanzugehörigkeit 
+  # Körperhöhe in Abhängigkeit von der Clanzugehörigkeit 
   orks <- orks %>%
     mutate(koerperhoehe_cm = case_when(clan == "Goffs" ~ round(rsnorm(n =100, mean = 180, sd = 10, xi = 15),2),
                                        clan == "Snakebites" ~ round(rsnorm(n =100, mean = 220, sd = 8, xi = 15),2),
